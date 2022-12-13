ba9289cd9ac8df2eae02c5d373dafc496cb7bfea
Raman Sailopal <rsailopal@robinsonbrothers.co.uk>
2022-12-13 09:16:23 +0000

    npm needs to be installed after new version of node

--- react/entrypoint/entrypoint.sh
+++ react/entrypoint/entrypoint.sh
@@ -4,5 +4,6 @@ apt-get install -y npm curl
 curl -sL https://deb.nodesource.com/setup_16.x | bash -
 apt-get install -y nodejs
 cd /home/react/fileman
+apt-get install -y npm
 npm install && npm start
 tail -f /dev/null
