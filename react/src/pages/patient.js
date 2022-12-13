8b7b9ef722ff857e861299c375c29162c7416a80
Raman Sailopal <rsailopal@robinsonbrothers.co.uk>
2022-12-13 12:09:13 +0000

    Added hospital numbers to display

--- react/src/pages/patient.js
+++ react/src/pages/patient.js
@@ -5,17 +5,21 @@ import { useParams } from 'react-router-dom';
 import '../index.css';
 const Patient = () => {
     const {id}=useParams();
+    var arr=[];
     let jsonstr= async() => {
         var x= await axios({url: process.env.REACT_APP_GITADD + '/fmqlEP?fmql=DESCRIBE%202-' + id });
-        console.log(x.data);
-        return x.data;
+        var y = await axios({url: process.env.REACT_APP_GITADD + '/fmqlEP?fmql=DESCRIBE 9000001-' + id });
+        arr = arr.concat(x.data.results[0]);
+        arr = arr.concat(y.data.results[0].health_record_no)
+        console.log(arr[1].value[0].health_record_no.value);
+        return arr;
     }
     jsonstr().then((data)=> {
         function cliked() {
             document.getElementById('root').style="display:none";
             window.location="/patients";
           }
-        if (data.results === undefined) {
+        if (data[0] === undefined) {
             ReactDOM.render(
                 <div class="errordiv">
                 <p align="center">
@@ -30,7 +34,7 @@ const Patient = () => {
         }
         else {
         let sx;
-        if (data.results[0].sex.value === "MALE") {
+        if (data[0].sex.value === "MALE") {
             sx="men"
         }
         else {
@@ -39,18 +43,26 @@ const Patient = () => {
         ReactDOM.render(
             <fieldset class="fieldset">
                 <form id="msform">
-                <img alt="" src={'https://randomuser.me/api/portraits/' + sx + '/' + data.results[0].uri.value.replace("2-","") + '.jpg'}/>
+                <img alt="" src={'https://randomuser.me/api/portraits/' + sx + '/' + data[0].uri.value.replace("2-","") + '.jpg'}/>
                 <label>ID:
                 <input
                     type="text" 
-                    value={data.results[0].uri.value.replace("2-","")}
+                    value={data[0].uri.value.replace("2-","")}
                     disabled="true"
                  />
                 </label><br></br><br></br>
                 <label>Name:
                 <input
                     type="text" 
-                    value={data.results[0].name.value}
+                    value={data[0].name.value}
+                    disabled="true"
+                    size="25"
+                 />
+                </label><br></br><br></br>
+                <label>Hospital Number:
+                <input
+                    type="text" 
+                    value={data[1].value[0].health_record_no.value}
                     disabled="true"
                     size="25"
                  />
@@ -58,77 +70,77 @@ const Patient = () => {
                 <label>Sex:
                 <input
                     type="text" 
-                    value={data.results[0].sex.value}
+                    value={data[0].sex.value}
                     disabled="true"
                  />
                 </label><br></br><br></br>
                 <label>Date of Birth:
                 <input
                     type="text" 
-                    value={data.results[0].date_of_birth.value.replace("T00:00:00Z","")}
+                    value={data[0].date_of_birth.value.replace("T00:00:00Z","")}
                     disabled="true"
                  />
                 </label><br></br><br></br>
                 <label>Social Security Number:
                 <input
                     type="text" 
-                    value={data.results[0].social_security_number.value}
+                    value={data[0].social_security_number.value}
                     disabled="true"
                  />
                 </label><br></br><br></br>
                 <label>Place of Birth City:
                 <input
                     type="text" 
-                    value={data.results[0].place_of_birth_city.value}
+                    value={data[0].place_of_birth_city.value}
                     disabled="true"
                  />
                 </label><br></br><br></br>
                 <label>Place of Birth State:
                 <input
                     type="text" 
-                    value={data.results[0].place_of_birth_state.sameAsLabel}
+                    value={data[0].place_of_birth_state.sameAsLabel}
                     disabled="true"
                  />
                 </label><br></br><br></br>
                 <label>Zip Code:
                 <input
                     type="text" 
-                    value={data.results[0].zip4===undefined ? "" : data.results[0].zip4.value}
+                    value={data[0].zip4===undefined ? "" : data[0].zip4.value}
                     disabled="true"
                  />
                 </label><br></br><br></br>
                 <label>Occupation:
                 <input
                     type="text" 
-                    value={data.results[0].occupation===undefined ? "" : data.results[0].occupation.value}
+                    value={data[0].occupation===undefined ? "" : data[0].occupation.value}
                     disabled="true"
                  />
                  </label><br></br><br></br>
                 <label>Phone number (residence):
                 <input
                     type="text" 
-                    value={data.results[0].phone_number_residence===undefined ? "" : data.results[0].phone_number_residence.value}
+                    value={data[0].phone_number_residence===undefined ? "" : data[0].phone_number_residence.value}
                     disabled="true"
                  />
                 </label><br></br><br></br>
                 <label>Phone number (Cell):
                 <input
                     type="text" 
-                    value={data.results[0].phone_number_cellular===undefined ? "" : data.results[0].phone_number_cellular.value}
+                    value={data[0].phone_number_cellular===undefined ? "" : data[0].phone_number_cellular.value}
                     disabled="true"
                  />
                 </label><br></br><br></br>
                 <label>Phone number (Work):
                 <input
                     type="text" 
-                    value={data.results[0].phone_number_work===undefined ? "" : data.results[0].phone_number_cellular.work}
+                    value={data[0].phone_number_work===undefined ? "" : data[0].phone_number_cellular.work}
                     disabled="true"
                  />
                 </label><br></br><br></br>
                 <label>Email Address:
                 <input
                     type="text" 
-                    value={data.results[0].email_address===undefined ? "" : data.results[0].email_address.value}
+                    value={data[0].email_address===undefined ? "" : data[0].email_address.value}
                     disabled="true"
                  />
                 </label><br></br><br></br>
--- react/src/pages/patientname.js
+++ react/src/pages/patientname.js
@@ -4,17 +4,22 @@ import axios from 'axios'
 import { useParams } from 'react-router-dom';
 const Patientname = () => {
     const {name}=useParams();
+    var arr=[];
     let jsonstr= async() => {
         var x= await axios({url: process.env.REACT_APP_GITADD + '/fmqlEP?fmql=DESCRIBE 2 FILTER (.01=' + name + ')' });
-        console.log(x.data);
-        return x.data;
+        var y = await axios({url: process.env.REACT_APP_GITADD + '/fmqlEP?fmql=DESCRIBE 9000001-' + x.data.results[0].uri.value.replace("2-","") });
+        arr = arr.concat(x.data.results[0]);
+        arr = arr.concat(y.data.results[0].health_record_no)
+        console.log(arr[1].value[0].health_record_no.value);
+        return arr;
+
     }
     jsonstr().then((data)=> {
         function cliked() {
             document.getElementById('root').style="display:none";
             window.location="/patients";
           }
-        if (data.results[0] === undefined) {
+        if (data[0] === undefined) {
             ReactDOM.render(
                 <div class="errordiv">
                 <p align="center">
@@ -29,7 +34,7 @@ const Patientname = () => {
         }
         else {
             let sx;
-            if (data.results[0].sex.value === "MALE") {
+            if (data[0].sex.value === "MALE") {
                 sx="men"
             }
             else {
@@ -38,97 +43,105 @@ const Patientname = () => {
         ReactDOM.render(
             <fieldset>
                 <form>
-                <img alt="" src={'https://randomuser.me/api/portraits/' + sx + '/' + data.results[0].uri.value.replace("2-","") + '.jpg'}/>
+                <img alt="" src={'https://randomuser.me/api/portraits/' + sx + '/' + data[0].uri.value.replace("2-","") + '.jpg'}/>
                 <label>ID:
                 <input
                     type="text" 
-                    value={data.results[0].uri.value.replace("2-","")}
-                    disabled="true"
+                    value={data[0].uri.value.replace("2-","")}
+                    disabled={true}
                  />
                 </label><br></br><br></br>
                 <label>Name:
                 <input
                     type="text" 
-                    value={data.results[0].name.value}
-                    disabled="true"
+                    value={data[0].name.value}
+                    disabled={true}
+                    size="25"
+                 />
+                </label><br></br><br></br>
+                <label>Hospital Number:
+                <input
+                    type="text" 
+                    value={data[1].value[0].health_record_no.value}
+                    disabled={true}
                     size="25"
                  />
                 </label><br></br><br></br>
                 <label>Sex:
                 <input
                     type="text" 
-                    value={data.results[0].sex.value}
-                    disabled="true"
+                    value={data[0].sex.value}
+                    disabled={true}
                  />
                 </label><br></br><br></br>
                 <label>Date of Birth:
                 <input
                     type="text" 
-                    value={data.results[0].date_of_birth.value.replace("T00:00:00Z","")}
-                    disabled="true"
+                    value={data[0].date_of_birth.value.replace("T00:00:00Z","")}
+                    disabled={true}
                  />
                 </label><br></br><br></br>
                 <label>Social Security Number:
                 <input
                     type="text" 
-                    value={data.results[0].social_security_number.value}
-                    disabled="true"
+                    value={data[0].social_security_number.value}
+                    disabled={true}
                  />
                 </label><br></br><br></br>
                 <label>Place of Birth City:
                 <input
                     type="text" 
-                    value={data.results[0].place_of_birth_city.value}
-                    disabled="true"
+                    value={data[0].place_of_birth_city.value}
+                    disabled={true}
                  />
                 </label><br></br><br></br>
                 <label>Place of Birth State:
                 <input
                     type="text" 
-                    value={data.results[0].place_of_birth_state.sameAsLabel}
-                    disabled="true"
+                    value={data[0].place_of_birth_state.sameAsLabel}
+                    disabled={true}
                  />
                 </label><br></br><br></br>
                 <label>Zip Code:
                 <input
                     type="text" 
-                    value={data.results[0].zip4===undefined ? "" : data.results[0].zip4.value}
-                    disabled="true"
+                    value={data[0].zip4===undefined ? "" : data[0].zip4.value}
+                    disabled={true}
                  />
                 </label><br></br><br></br>
                 <label>Occupation:
                 <input
                     type="text" 
-                    value={data.results[0].occupation===undefined ? "" : data.results[0].occupation.value}
-                    disabled="true"
+                    value={data[0].occupation===undefined ? "" : data[0].occupation.value}
+                    disabled={true}
                  />
                  </label><br></br><br></br>
                 <label>Phone number (residence):
                 <input
                     type="text" 
-                    value={data.results[0].phone_number_residence===undefined ? "" : data.results[0].phone_number_residence.value}
-                    disabled="true"
+                    value={data[0].phone_number_residence===undefined ? "" : data[0].phone_number_residence.value}
+                    disabled={true}
                  />
                 </label><br></br><br></br>
                 <label>Phone number (Cell):
                 <input
                     type="text" 
-                    value={data.results[0].phone_number_cellular===undefined ? "" : data.results[0].phone_number_cellular.value}
-                    disabled="true"
+                    value={data[0].phone_number_cellular===undefined ? "" : data[0].phone_number_cellular.value}
+                    disabled={true}
                  />
                 </label><br></br><br></br>
                 <label>Phone number (Work):
                 <input
                     type="text" 
-                    value={data.results[0].phone_number_work===undefined ? "" : data.results[0].phone_number_cellular.work}
-                    disabled="true"
+                    value={data[0].phone_number_work===undefined ? "" : data[0].phone_number_cellular.work}
+                    disabled={true}
                  />
                 </label><br></br><br></br>
                 <label>Email Address:
                 <input
                     type="text" 
-                    value={data.results[0].email_address===undefined ? "" : data.results[0].email_address.value}
-                    disabled="true"
+                    value={data[0].email_address===undefined ? "" : data[0].email_address.value}
+                    disabled={true}
                  />
                 </label><br></br><br></br>
                 </form>
