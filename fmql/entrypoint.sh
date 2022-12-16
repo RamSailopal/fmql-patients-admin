2230900dc3444a74d6ddadd025232985a06360a9
Raman Sailopal <rsailopal@robinsonbrothers.co.uk>
2022-12-16 16:17:08 +0000

    Added CORS to npm install

--- fmql/entrypoint.sh
+++ fmql/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cp -f /home/fmqlput/fmqlServer.js /home/vdp/FMQL/webservice/fmqlServer.js
+cd /home/vdp/FMQL/webservice
+npm install cors
 su vdp -c "source /home/nodevista/etc/env && pm2 start /home/vdp/FMQL/webservice/fmqlServer.js > /home/vdp/logs/nodelog.log"
 /home/nodevista/bin/start.sh
